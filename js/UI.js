4c2324aea31a957ca985b78c56bbff30cb1b0530
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-12-15 15:38:25 -0500

    nav loads content and content scrolls within its own section

--- js/UI.js
+++ js/UI.js
@@ -1,10 +1,11 @@
 
 
-
 $(document).ready(function() {
 
   $("#content").load("content/main.html");
 
+
+//make this one generic function that handles them all
   $("#main").on("click", function() {
   	$("#content").empty();
   	$("#content").load("content/main.html");
@@ -15,5 +16,31 @@ $(document).ready(function() {
   	$("#content").load("content/blog.html");
   });
 
+  $("#apps").on("click", function() {
+  	$("#content").empty();
+  	$("#content").load("content/apps.html");
+  });
+
+  $("#music").on("click", function() {
+  	$("#content").empty();
+  	$("#content").load("content/music.html");
+  });
+
+  $("#stories").on("click", function() {
+  	$("#content").empty();
+  	$("#content").load("content/stories.html");
+  });
+
+  $("#bio").on("click", function() {
+  	$("#content").empty();
+  	$("#content").load("content/bio.html");
+  });
+
 
 });
+
+
+//down here
+function switchContent() {
+
+}
