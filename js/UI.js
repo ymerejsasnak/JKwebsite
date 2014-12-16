f465ce5ea5cbd7f959d7420bad17eb519f2491c8
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-12-15 22:21:30 -0500

    more tweaks and stuff...main thing is condensed jquery nav into single function

--- js/UI.js
+++ js/UI.js
@@ -5,42 +5,16 @@ $(document).ready(function() {
   $("#content").load("content/main.html");
 
 
-//make this one generic function that handles them all
-  $("#main").on("click", function() {
-  	$("#content").empty();
-  	$("#content").load("content/main.html");
-  });
-
-  $("#blog").on("click", function() {
-  	$("#content").empty();
-  	$("#content").load("content/blog.html");
-  });
-
-  $("#apps").on("click", function() {
-  	$("#content").empty();
-  	$("#content").load("content/apps.html");
-  });
-
-  $("#music").on("click", function() {
-  	$("#content").empty();
-  	$("#content").load("content/music.html");
-  });
-
-  $("#stories").on("click", function() {
-  	$("#content").empty();
-  	$("#content").load("content/stories.html");
-  });
-
-  $("#bio").on("click", function() {
-  	$("#content").empty();
-  	$("#content").load("content/bio.html");
+  $(".nav-link").on("click", function() {
+  	switchContent($(this).attr("id"));  	
   });
 
 
 });
 
 
-//down here
-function switchContent() {
 
+function switchContent(content) {
+	$("#content").empty();
+	$("#content").load("content/" + content + ".html");
 }
