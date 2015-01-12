a2a93c2b7fb81ed96d339429ca5f684df8cf329e
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-01-11 22:22:53 -0500

    basic reset done, now can work on design more and start filling in content

--- README.md
+++ README.md
@@ -9,25 +9,11 @@
 
 
 
-
-starting the long slow process of creating my web site<br>
-
-going to use this as an opportunity to practice as much as possible<br>
-
-but especially practice stuff I haven't used much/at all: media queries, transitions, grid layout,
-
-
 TO DO: 
 make all img as small as possible filesize-wise
 start filling in content
 -add seperate link to good example web pages I've made too? (for portfolio purposes)
 -add link to my github on about, maybe other stuff just for fun like goodreads, etc.
-
-??
- eventually add js to make text (at least main header) randomly weird!-gets more random more you click, make function for this that can be called as part of any action...start it off mostly normal...never 100% the same from beginning though...run this on other elements too? -->
-
-
-
 
 
 
--- js/UI.js
+++ js/UI.js
@@ -3,10 +3,7 @@
 $(document).ready(function() {
 
   //initial loading of main content section
-  $("#content").load("content/main.html", function() {
-    var text = $("#content p").text();
-  	$("#content p").html(textEffects(text));
-  });
+  $("#content").load("content/main.html");
 
 
   //loads whatever is clicked on
@@ -25,36 +22,5 @@ $(document).ready(function() {
 function switchContent(pageContent) {
 	var context = $("#content");
 	context.empty();
-	context.load("content/" + pageContent + ".html", function() {
-		var text = $("#content p").text();  //will need to handle multiple p tags eventually --use each?
-	  $("#content p").html(textEffects(text));
-	});
-}
-
-
-
-
-function textEffects(text) {
-	var symbols = ["!", "=", ".", "'"];
-	var words = text.split(" ");
-	words.forEach(function(word, index) {
-    if (Math.floor(Math.random() * 2) === 0) { //how often a word is affected
-
-    	switch(Math.floor(Math.random() * 2)) { 
-
-        case 0:
-          words[index] = "<span class='tilt-left'>" + word + "</span>"
-          break;
-        case 1:
-          words[index] = "<span class='tilt-right'>" + word + "</span>"
-          break;
-                  
-	    } 
-	    
-
-	  }
-
-	});
-
-	return words.join(" "); 
+	context.load("content/" + pageContent + ".html");
 }
